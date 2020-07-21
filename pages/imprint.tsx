07b1daea5f6527867c326d8ae9e25bfd736e72cc
bs1180 <benedictsmith@gmail.com>
2020-07-21 11:28:52 +0000

    Add basic imprint

---
+++ pages/imprint.tsx
@@ -0,0 +1,10 @@
+const Imprint = () => {
+  return <div>
+      <div className="">
+      <h3>International Improv Community in Vienna</h3>
+      <div>ZVR-Number: 1236421179</div>
+      </div>
+  </div>;
+};
+
+export default Imprint;
