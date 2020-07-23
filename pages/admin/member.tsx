7b94b94ceb387446d05e81ebbbaa8f3741a99974
Ben Smith <benedictsmith@gmail.com>
2020-07-23 17:14:49 +0200

    Remove unused pages

--- pages/admin/member.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function MemberPage() {
-  return (
-    <div className="">
-      <div className="mx-auto max-w-lg">Member details. Subscription detail. Add a note.</div>
-    </div>
-  );
-}
--- pages/admin/members.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function MembersPage() {
-  return (
-    <div className="">
-      <div className="mx-auto max-w-lg">Table with list of members</div>
-    </div>
-  );
-}
