08025edf56d7f0ecccdcce84346dfa12af5f55d3
Ben Smith <benedictsmith@gmail.com>
2021-08-19 17:26:26 +0200

    Fix stripe

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2020-03-02",
+  apiVersion: "2020-08-27",
 });
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
